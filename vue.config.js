4f4d06b7d5c25b45aba53441f0d2b52254e62ad8
David Hedgren <david@hedgren.se>
2021-12-28 20:59:36 +0100

    feat(devx): streamlines the theming

--- vue.config.js
+++ vue.config.js
@@ -11,7 +11,7 @@ module.exports = {
   css: {
     loaderOptions: {
       scss: {
-        additionalData: `@import "~@/variables.scss";`,
+        // additionalData: `@import "~@/variables.scss";`,
       },
     },
   },
