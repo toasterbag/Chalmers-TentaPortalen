b23495dea0f3356006b9599343162d26c6cfae33
David Hedgren <david@hedgren.se>
2021-12-28 16:00:47 +0100

    fix(security): added admin guard to the import trigger route

--- src/routes/admin/trigger_survey_scan.ts
+++ src/routes/admin/trigger_survey_scan.ts
@@ -7,6 +7,7 @@ import { scrape_everything } from "@app/import/study_portal";
 export default {
   method: Method.GET,
   path: "/admin/material/scan",
+  auth: ["admin"],
 
   handler: async (_: Request, ctx: Context): Promise<Response> => {
     scrape_everything(ctx);
